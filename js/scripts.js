6ae99a1aec498a27108d760fc33b2cab097bada9
MiguelSecadesBYU <sec21002@byui.edu>
2024-02-07 11:48:44 +0100

    last modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const year = document.querySelector('#year');
 year.innerHTML = new Date().getFullYear();
 
+document.getElementById("modify").innerHTML = document.lastModified
+
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
